112c4202e818d0c05e2b0b9248a166cf03c0063a
keanlouis30 <keanlouis30@gmail.com>
2024-11-20 19:58:57 +0800

    Create mp.c

---
+++ mp.c
@@ -0,0 +1,5 @@
+// Rosales, Kean Louis R.
+// Albarracin, Clarissa
+// This project was made in compliance to the partial requirements
+// of the course LBYARCH under Sir Carlo Adriano
+// Submitted this day of: 
